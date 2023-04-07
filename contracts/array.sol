6a25dd809da785b6a14358833c4a2579f8429f7c
Xlaez <utibeabasiekong2@gmail.com>
2023-04-07 22:47:35 +0100

    added new features

--- contracts/array.sol
+++ contracts/array.sol
@@ -2,9 +2,9 @@
 pragma solidity ^0.8.18;
 
 contract Array{
-    //Ways to initialize arrays
-    uint[] public arr;
-    uint[] public arr2 = [1, 2, 3, 4];
+    //Ways to initializ
+    uint[] public arr2 = e arrays
+    uint[] public arr;[1, 2, 3, 4];
     // Fixed size array, all elements are iitialized to zero
     uint[12] public fixedSizeArr; 
 
